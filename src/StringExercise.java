53072fa17cdf21fa1b5aaf0615d5c8e0636eac9b
Sam Bowcut <sbowcut@gmail.com>
2021-07-01 08:46:14 -0600

    7-1-21

---
+++ src/StringExercise.java
@@ -0,0 +1,27 @@
+public class StringExercise {
+
+    public static void main(String[] args) {
+
+//        1. String basics
+
+        String pinkFloyd = "We don't need no education\n" +
+                "We don't need no thought control";
+
+        System.out.println(pinkFloyd);
+
+        String checkThis = "Check \"this\" out!, \"s inside of \"s!";
+
+        System.out.println(checkThis);
+
+        String cDrive = "In windows, the main drive is usually C:\\";
+
+        System.out.println(cDrive);
+
+        String hackNSlash = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+
+        System.out.println(hackNSlash);
+
+    }
+
+}
